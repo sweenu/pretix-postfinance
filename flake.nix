9862fad4ae9073f24c40741c0067b129bdfd9488
sweenu <contact@sweenu.xyz>
2026-01-20 21:34:01 +0100

    Release: 1.1.0

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
         {
           default = pyPkgs.buildPythonPackage {
             pname = "pretix-postfinance";
-            version = "1.0.1";
+            version = "1.1.0";
             src = self;
             format = "pyproject";
 
